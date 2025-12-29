92fcf3a900151ffc7d3bcda0a52df10e58e1c4a1
netoguild-rgb <netoguild@gmail.com>
2025-12-28 22:43:42 -0300

    css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// Fontes
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <--- ESSA LINHA É A MAIS IMPORTANTE!
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
