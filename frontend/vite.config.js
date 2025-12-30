0de4360a42e7454f1c6b603814552ace9b15b3ab
netoguild-rgb <netoguild@gmail.com>
2025-12-29 22:29:13 -0300

    f

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Isto ajuda a evitar a tela branca com gráficos
+  optimizeDeps: {
+    include: ['chart.js', 'react-chartjs-2']
+  }
 })
